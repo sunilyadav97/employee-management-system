84d29f08f3c1f4a410a034d571fe57b3bcbf9c53
Sunil Yadav <sunil@educationloanguru.com>
2024-07-10 18:18:08 +0530

    Added str function

--- emsapp/models.py
+++ emsapp/models.py
@@ -73,3 +73,7 @@ class Performance(TimeStampedModel):
     rating = models.DecimalField(max_digits=10, decimal_places=2)
     comments = models.TextField()
     is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.employee.user.get_full_name()
+    
