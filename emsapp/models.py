ab63d67e83ca08315d32b88c95a55af00be6b055
Sunil Yadav <sunil@educationloanguru.com>
2024-07-10 17:54:35 +0530

    Created all models.

--- emsapp/models.py
+++ emsapp/models.py
@@ -1,1 +1,75 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Department(TimeStampedModel):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(TimeStampedModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    mobile_no = models.CharField(max_length=100, null=True, blank=True)
+    dob = models.DateField()
+    position = models.CharField(max_length=100, null=True, blank=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    address = models.TextField(null=True, blank=True)
+
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        self.user.get_full_name()
+
+
+class PayRoll(TimeStampedModel):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    basic_salary = models.DecimalField(max_digits=10, decimal_places=2)
+    bonuses = models.DecimalField(max_digits=10, decimal_places=2)
+    deductions = models.DecimalField(max_digits=10, decimal_places=2)
+    net_salary = models.DecimalField(max_digits=10, decimal_places=2)
+
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        self.employee.user.get_full_name()
+
+
+class Attendance(TimeStampedModel):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    date = models.DateField()
+    is_present = models.BooleanField(default=False)
+
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.date
+
+
+class Leave(TimeStampedModel):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    leave_type = models.CharField(max_length=100)
+    from_date = models.DateField()
+    to_date = models.DateField()
+    reason = models.TextField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.leave_type
+
+
+class Performance(TimeStampedModel):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    rating = models.DecimalField(max_digits=10, decimal_places=2)
+    comments = models.TextField()
+    is_active = models.BooleanField(default=True)
